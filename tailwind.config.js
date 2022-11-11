0ba7bccf190e28658606d879cd67c740edf646cf
andiedoescode <pang.andrea@gmail.com>
2022-11-11 15:50:17 -0500

    Added basic styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
-  theme: {
-    colors: {},
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-    },
-    extend: {
-      flexGrow: {
-        2: '2',
-        3: '3',
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#1a7de5",
+          "secondary": "#008a62",
+          "accent": "#c86003",
+          "neutral": "#424656",
+          "base-100": "#F2F2F8",
+          "info": "#82A3D9",
+          "success": "#00984c",
+          "warning": "#F2A63A",
+          "error": "#F17165",
+        },
       },
-    }
+    ],
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
 }
