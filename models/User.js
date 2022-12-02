83d631c40a45bfba080a9b2da49dea6a795dcd7d
andiedoescode <pang.andrea@gmail.com>
2022-12-01 23:16:18 -0500

    Edit profile view and put routes

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   userName: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
-  ashaId: String,
+  profId: String,
 });
 
 // Password hashing middleware.
