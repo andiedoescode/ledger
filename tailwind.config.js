710cbbd0436db5129fe95ea613387f299aa68967
andiedoescode <pang.andrea@gmail.com>
2022-12-08 13:39:35 -0500

    Display profile pic on profile page and edit page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,12 @@ module.exports = {
           "neutral": "#424656",
           "base-100": "#E0E0E5",
           "info": "#82A3D9",
+          // "info": "#0D0630",
           "success": "#00984c",
           "warning": "#F2A63A",
           "error": "#F17165",
+          "darken": "#0D0630",
+
         },
       },
     ],
