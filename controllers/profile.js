635baf972e701f6a20dc0a5cef94b2ba0d7ad5eb
andiedoescode <pang.andrea@gmail.com>
2022-12-14 18:50:06 -0500

    Added backend to calculate total hours and courses completed

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,10 +6,26 @@ module.exports = {
   //GET /profile/:id
   getProfile: async (req, res) => {
     try {
-      const courses = await Course.find({ user: req.user.id });
-      res.render("profile.ejs", { courses: courses, user: req.user });
+      const courses = await Course.find({ createdById: req.user.id });
+    
+      //Get total number of courses completed
+      const totalCourses = courses.length
+
+      //Get total number of hours completed
+      let totalHrs = 0
+      courses.forEach(course => totalHrs += course.ceLength)
+
+
+      res.render("profile.ejs", { 
+        courses: courses, 
+        totalCourses: totalCourses, 
+        totalHrs: totalHrs,
+        user: req.user, 
+      });
+
     } catch (err) {
-      console.log(err);
+      // console.log(err);
+      if (err) return res.status(500).send(err.toString());
     }
   },
 
@@ -19,7 +35,10 @@ module.exports = {
       const profile = await User.findOne({ _id: req.user.id }).lean()
 
       if (profile._id == req.user.id) {
-        res.render("editprofile.ejs", { user: req.user })
+        res.render("editprofile.ejs", { 
+          user: req.user 
+        });
+        
       } else {
         res.redirect("/dashboard")
       }
