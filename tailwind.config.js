bd89be81e2b05e793393da3da2c2de18706fd72f
andiedoescode <pang.andrea@gmail.com>
2022-12-22 21:23:23 -0500

    #32 Added optional target hours for users and live data visualization to dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,16 @@ module.exports = {
           "secondary": "#008a62",
           "accent": "#c86003",
           "neutral": "#424656",
-          // "base-100": "#E0E0E5",
           "base-100": "#f3f4f6",
           "info": "#82A3D9",
-          // "info": "#0D0630",
           "success": "#00984c",
           "warning": "#F2A63A",
           "error": "#ea580c",
           "darken": "#0D0630",
-
+          // "base-100": "#E0E0E5",
+          // "info": "#0D0630",
         },
-        container: {
-          center: true,
-        }
+
       },
     ],
   },
